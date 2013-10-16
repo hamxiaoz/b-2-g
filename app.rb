118abdc348e18a5e1b6810adde574e7be27db287
Andrew Zheng <zheng3891@gmail.com>
2013-10-15 21:18:53 -0700

    debug post body

--- app.rb
+++ app.rb
@@ -11,6 +11,10 @@ post '/commits' do
   # https://confluence.atlassian.com/display/BITBUCKET/POST+hook+management
   # The payload has payload= prepended to the actual payload. 
   # The payload is url encode content.
-  json = JSON.parse(URI.unescape(request.body.read().sub(/^payload=/, "")))
-  puts json.to_s
+  body = request.body.read()
+  puts body
+  body_clean = body.sub /^payload=/, ""
+  puts body_clean
+  # json = JSON.parse(URI.unescape(request.body.read().sub(/^payload=/, "")))
+  # puts json.to_s
 end
