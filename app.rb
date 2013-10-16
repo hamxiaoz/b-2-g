19e9ed3c6f9ec31bcad0db45ea0ef3cbd318ce1b
Andrew Zheng <zheng3891@gmail.com>
2013-10-15 21:09:30 -0700

    parse json in payload

--- app.rb
+++ app.rb
@@ -1,11 +1,16 @@
 require 'sinatra'
 require 'json'
+require 'uri'
 
 get '/' do
   'Hello world!'
 end
 
 post '/commits' do
-  # TODO
-  puts JSON.parse(request.body.read()).to_s
+  # payload format
+  # https://confluence.atlassian.com/display/BITBUCKET/POST+hook+management
+  # The payload has payload= prepended to the actual payload. 
+  # The payload is url encode content.
+  json = JSON.parse(URI.unescape(request.body.read().sub(/^payload=/, "")))
+  puts json.to_s
 end
