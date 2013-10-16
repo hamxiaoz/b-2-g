9da953d3c8622b6f46ffadad943f0d94842b7199
Andrew Zheng <zheng3891@gmail.com>
2013-10-15 21:29:50 -0700

    use cgi to decode

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
 require 'json'
-require 'uri'
+require 'cgi'
 
 get '/' do
   'Hello world!'
@@ -11,10 +11,6 @@ post '/commits' do
   # https://confluence.atlassian.com/display/BITBUCKET/POST+hook+management
   # The payload has payload= prepended to the actual payload. 
   # The payload is url encode content.
-  body = request.body.read()
-  puts body
-  body_clean = body.sub /^payload=/, ""
-  puts body_clean
-  # json = JSON.parse(URI.unescape(request.body.read().sub(/^payload=/, "")))
-  # puts json.to_s
+  json = JSON.parse(CGI.unescape(request.body.read().sub(/^payload=/, "")))
+  puts json.to_s
 end
