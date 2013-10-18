122e8f4014547677910a81183de33d79bc3da001
Andrew Zheng <zheng3891@gmail.com>
2013-10-18 01:22:42 -0700

    modular sinatra

--- app.rb
+++ app.rb
@@ -5,31 +5,32 @@ require 'cgi'
 require 'octokit'
 # require 'figaro'
 
-# class Application < Sinatra::Base
-get '/' do
-  'Hi'
-end
+class Application < Sinatra::Base
+  get '/' do
+    'Hi'
+  end
 
-REPO = "hamxiaoz/bitbucket-activity"
-LOG_FILE = "log"
+  REPO = "hamxiaoz/bitbucket-activity"
+  LOG_FILE = "log"
 
-post '/commits' do
-  # payload format
-  # https://confluence.atlassian.com/display/BITBUCKET/POST+hook+management
-  # The payload has payload= prepended to the actual payload. 
-  # The payload is url encode content.
-  json = JSON.parse(CGI.unescape(request.body.read().sub(/^payload=/, "")))
-  msg = json['commits'].map do |c|
-        # obfuscate message with '*'
-        obfuscated = c['message'].gsub /[^\n\r]/, "*"
-        "#{c['utctimestamp']}, #{obfuscated}"
-        end.join
-  puts msg
+  post '/commits' do
+    # payload format
+    # https://confluence.atlassian.com/display/BITBUCKET/POST+hook+management
+    # The payload has payload= prepended to the actual payload. 
+    # The payload is url encode content.
+    json = JSON.parse(CGI.unescape(request.body.read().sub(/^payload=/, "")))
+    msg = json['commits'].map do |c|
+          # obfuscate message with '*'
+          obfuscated = c['message'].gsub /[^\n\r]/, "*"
+          "#{c['utctimestamp']}, #{obfuscated}"
+          end.join
+    puts msg
 
-  # replace file content with msg and commit with msg
-  # client = Octokit::Client.new ENV['ACCESS_TOKEN']
-  # sha = client.contents(REPO, :path => LOG_FILE)[:sha]
-  # client.update_contents(REPO, LOG_FILE, msg, sha, msg)
+    # replace file content with msg and commit with msg
+    # client = Octokit::Client.new ENV['ACCESS_TOKEN']
+    # sha = client.contents(REPO, :path => LOG_FILE)[:sha]
+    # client.update_contents(REPO, LOG_FILE, msg, sha, msg)
+  end
 end
 
 # if Application.settings.development?
