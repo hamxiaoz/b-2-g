17169b00c73c928cd56d65c0b62a0248a18f9630
Andrew Zheng <zheng3891@gmail.com>
2013-10-18 00:28:47 -0700

    connects to github; use figaro to store settings

--- app.rb
+++ app.rb
@@ -1,21 +1,48 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'json'
 require 'cgi'
+require 'octokit'
+require 'figaro'
 
-get '/' do
-  'Hello world!'
+class Application < Sinatra::Base
+  get '/' do
+    'Hi'
+  end
+
+  REPO = "hamxiaoz/bitbucket-activity"
+  LOG_FILE = "log"
+
+  post '/commits' do
+    # payload format
+    # https://confluence.atlassian.com/display/BITBUCKET/POST+hook+management
+    # The payload has payload= prepended to the actual payload. 
+    # The payload is url encode content.
+    json = JSON.parse(CGI.unescape(request.body.read().sub(/^payload=/, "")))
+    msg = json['commits'].map do |c|
+          # obfuscate message with '*'
+          obfuscated = c['message'].gsub /[^\n\r]/, "*"
+          "#{c['utctimestamp']}, #{obfuscated}"
+          end.join
+    puts msg
+
+    # replace file content with msg and commit with msg
+    client = Octokit::Client.new ENV['ACCESS_TOKEN']
+    sha = client.contents(REPO, :path => LOG_FILE)[:sha]
+    client.update_contents(REPO, LOG_FILE, msg, sha, msg)
+  end
 end
 
-post '/commits' do
-  # payload format
-  # https://confluence.atlassian.com/display/BITBUCKET/POST+hook+management
-  # The payload has payload= prepended to the actual payload. 
-  # The payload is url encode content.
-  json = JSON.parse(CGI.unescape(request.body.read().sub(/^payload=/, "")))
-  msg = json['commits'].map do |c|
-        # obfuscate message with '*'
-        obfuscated = c['message'].gsub /[^\n\r]/, "*"
-        "#{c['utctimestamp']}, #{obfuscated}"
-        end.join
-  puts msg
+# figaro with sinatra, see https://github.com/laserlemon/figaro/issues/60
+module Figaro
+  def path
+    @path ||= File.join(Application.settings.root, "config", "application.yml")
+  end
+
+  def environment
+    Application.settings.environment
+  end
+end
+
+Figaro.env.each do |key, value|
+  ENV[key] = value unless ENV.key?(key)
 end
