a094b0033e1f1e7f0c4002e8cfb5b97d831226de
Andrew Zheng <zheng3891@gmail.com>
2013-10-19 12:27:24 -0700

    log repo name also, updated readme

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
-b-2-g
-=====
-
+# b-2-g
 Bitbucket activity transform to Github activity
+
+# How it works
+1. Deploy this app to Heroku
+2. Setup Bitbucket 'POST' hook to post to the app
+3. When post receives, the app update the 'LOG' file content and commit to Github.
+
+# How to deploy
+1. Create a new Personal Access Tokens from github
+2. Deploy to Heroku
+
+    ```ruby
+    git clone
+    # change REPO name and LOG_FILE name in app.rb
+    hekrou create
+    heroku config:add WELCOME_MSG=xxxxxxx
+    heroku config:add ACCESS_TOKEN=xxxxxxx
+    ```
+    
+3. Setup 'POST' hook in Bitbucket to your Heroku app:
+`xxxxx.herokuapp.com/commits`
+
--- app.rb
+++ app.rb
@@ -24,9 +24,10 @@ class Application < Sinatra::Base
     # The payload is url encode content.
     json = JSON.parse(CGI.unescape(request.body.read().sub(/^payload=/, "")))
     msg = json['commits'].map do |c|
-          # obfuscate message with '*'
-          obfuscated = c['message'].gsub /[^\n\r]/, "*"
-          "#{c['utctimestamp']}, #{obfuscated}"
+            # obfuscate message with '*'
+            obfuscated_msg = c['message'].gsub /[^\n\r]/, "*"
+            obfuscated_repo_name = c['repository']['name'].gsub /[A-Za-z0-9]/, "x"
+            "#{c['utctimestamp']}, [#{obfuscated_repo_name}], #{obfuscated_msg}"
           end.join
 
     # replace file content with msg and commit with msg
